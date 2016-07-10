32dec041ebc1c83ae37fd652560baaed4154c508
smilecold <691823598@qq.com>
2016-07-10 10:05:59 +0800

    test

---
+++ app/src/main/java/com/lxziih/miniweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lxziih.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lxzii on 2016/7/10.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
