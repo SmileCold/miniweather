8352a0b1ff2a8c0dd31cdd2707ece5491417f1f4
smilecold <691823598@qq.com>
2016-07-10 10:59:02 +0800

    网网络状态检查

--- app/src/main/java/com/lxziih/miniweather/MainActivity.java
+++ app/src/main/java/com/lxziih/miniweather/MainActivity.java
@@ -2,6 +2,10 @@ package com.lxziih.miniweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.pku.lxziih.util.NetUtil;
 
 /**
  * Created by lxzii on 2016/7/10.
@@ -12,5 +16,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+            Log.d("myWeather", "网络OK");
+            Toast.makeText(MainActivity.this,"网络OK！", Toast.LENGTH_LONG).show();
+        }else
+        {
+            Log.d("myWeather", "网络挂了");
+            Toast.makeText(MainActivity.this,"网络挂了！", Toast.LENGTH_LONG).show();
+        }
     }
 }
